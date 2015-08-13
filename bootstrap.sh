b5130da791efa83a2aecd2831153c0de0ece2981
Mada Aryakusumah <mada@41studio.com>
2015-08-13 15:56:28 +0700

    add elasticsearch

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,9 @@ apt-add-repository -y ppa:brightbox/ruby-ng >/dev/null 2>&1
 apt-get -y update >/dev/null 2>&1
 
 install 'development tools' build-essential
+install 'Nokogiri dependencies' libxml2 libxml2-dev libxslt1-dev zlib1g-dev
+install 'Java' default-jre default-jdk
+install 'ExecJS runtime' nodejs
 
 install Ruby ruby2.2 ruby2.2-dev
 update-alternatives --set ruby /usr/bin/ruby2.2 >/dev/null 2>&1
@@ -19,17 +22,28 @@ update-alternatives --set gem /usr/bin/gem2.2 >/dev/null 2>&1
 echo installing Bundler
 gem install bundler -N >/dev/null 2>&1
 
+echo installing Rails
+gem install rails --no-ri --no-rdoc
+
 install Git git
 install SQLite sqlite3 libsqlite3-dev
 install memcached memcached
 install Redis redis-server
-install RabbitMQ rabbitmq-server
+install ImageMagick imagemagick
 
 install PostgreSQL postgresql postgresql-contrib libpq-dev
 sudo -u postgres createuser --superuser vagrant
 sudo -u postgres createdb -O vagrant activerecord_unittest
 sudo -u postgres createdb -O vagrant activerecord_unittest2
+sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password 'postgres';"
 
+echo installing Elasticsearch
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.5.2.deb
+sudo dpkg -i elasticsearch-1.5.2.deb
+sudo update-rc.d elasticsearch defaults 95 10
+sudo /etc/init.d/elasticsearch start
+
+# Setup my sql
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 install MySQL mysql-server libmysqlclient-dev
@@ -41,9 +55,6 @@ GRANT ALL PRIVILEGES ON activerecord_unittest.* to 'rails'@'localhost';
 GRANT ALL PRIVILEGES ON activerecord_unittest2.* to 'rails'@'localhost';
 GRANT ALL PRIVILEGES ON inexistent_activerecord_unittest.* to 'rails'@'localhost';
 SQL
-
-install 'Nokogiri dependencies' libxml2 libxml2-dev libxslt1-dev
-install 'ExecJS runtime' nodejs
 
 # Needed for docs generation.
 update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
